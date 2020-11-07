8bb19540ec9ab4246486246de84e979c15f6d7de
taka <takahashi.toshinari.dev00@gmail.com>
2020-11-08 06:25:45 +0900

    :bulb: :sparkles: utilsにget_ttl_valueのガワを追加&sample.pyにコメントを追加

--- plapo/utils.py
+++ plapo/utils.py
@@ -1,7 +1,20 @@
 import random
 import string
 
+from numpy.core import number
+
 
 def get_random_string(length: int):
     randlist = [random.choice(string.digits) for i in range(length)]
     return ''.join(randlist)
+
+
+def get_ttl_value(days: int) -> number:
+    """
+    日数を指定するとunixtimeに変換してttl値を返す
+
+    :param days:
+    :return: ttl値(unixtime)
+    """
+    # TODO: 実装書く
+    return
